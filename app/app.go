e0c6912bd5ec03938df9e9ef42024cb5273c5a04
fahmiidris <fahmiidris@apps.ipb.ac.id>
2024-01-29 22:24:52 +0700

    feat : CRUD (create, read, update and delete book)

--- app/app.go
+++ app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"book-inventory/models"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,5 +24,78 @@ func (h *Handler) GetBooks(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"title":   "Book Inventory",
 		"payload": books,
+		"auth":    c.Query("auth"),
 	})
+}
+
+func (h *Handler) GetBookById(c *gin.Context) {
+	var book models.Books
+	id := c.Param("id")
+	if h.DB.Find(&book, "id=?", id).RecordNotFound() {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
+	c.HTML(http.StatusOK, "book.html", gin.H{
+		"title":   book.Title,
+		"payload": book,
+		"auth":    c.Query("auth"),
+	})
+}
+
+func (h *Handler) AddBook(c *gin.Context) {
+	c.HTML(http.StatusOK, "form.html", gin.H{
+		"title": "Add Book",
+		"auth":  c.Query("auth"),
+	})
+}
+
+func (h *Handler) PostBook(c *gin.Context) {
+	var book models.Books
+	err := c.Bind(&book)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	h.DB.Create(&book)
+	c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("/books?auth=%s", c.PostForm("auth")))
+}
+
+func (h *Handler) UpdateBook(c *gin.Context) {
+	var books models.Books
+
+	bookId := c.Param("id")
+	if h.DB.Find(&books, "id=?", bookId).RecordNotFound() {
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{
+			"error": "not found"})
+	}
+
+	c.HTML(http.StatusOK, "form.html", gin.H{
+		"title":   "add book",
+		"payload": books,
+		"auth":    c.Query("auth"),
+	})
+}
+
+func (h *Handler) PutBook(c *gin.Context) {
+	var books models.Books
+
+	bookId := c.Param("id")
+	if h.DB.Find(&books, "id=?", bookId).RecordNotFound() {
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{
+			"error": "not found"})
+	}
+
+	var reqBook = books
+	c.Bind(&reqBook)
+
+	h.DB.Model(&books).Where("id=?", bookId).Update(reqBook)
+
+	c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("/book/%s?auth=%s", bookId, c.PostForm("auth")))
+}
+
+func (h *Handler) DeleteBook(c *gin.Context) {
+	var books models.Books
+
+	bookId := c.Param("id")
+	h.DB.Delete(&books, "id=?", bookId)
+
+	c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("/books?auth=%s", c.PostForm("auth")))
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"book-inventory/app"
 	"book-inventory/auth"
 	"book-inventory/db"
+	"book-inventory/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,6 +27,20 @@ func main() {
 
 	//get all books
 	router.GET("/books", handler.GetBooks)
+
+	// get book by id
+	router.GET("/book/:id", middleware.AuthValidation, handler.GetBookById)
+
+	// add book
+	router.GET("/addBook", middleware.AuthValidation, handler.AddBook)
+	router.POST("/book", middleware.AuthValidation, handler.PostBook)
+
+	// update book
+	router.GET("/updateBook/:id", middleware.AuthValidation, handler.UpdateBook)
+	router.POST("/updateBook/:id", middleware.AuthValidation, handler.PutBook)
+
+	// delete book
+	router.POST("/deleteBook/:id", middleware.AuthValidation, handler.DeleteBook)
 
 	router.Run(":5000")
 }
